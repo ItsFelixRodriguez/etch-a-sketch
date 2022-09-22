f6a880d115a8f3c86532b915b1205c1f5f1bbfd9
ItsFelixRodriguez <ItsFelixRodriguez@gmail.com>
2022-09-22 17:00:34 +0900

    Still can't get squares to draw instead of lines

--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 // app
-
+let color = '';
 function populateBoard (size) {
-    let board = document.querySelector('.board');
+    let board = document.querySelector(".board");
     let squares = board.querySelectorAll("div");
-    squares.forEach(div =>  div.remove());
+    squares.forEach((div) =>  div.remove());
     board.style.gridTemplateColumns = `repeat (${size}, 1fr)`;
     board.style.gridTemplateRows = `repeat (${size}, 1fr)`;
-    
-    let amount = size*size
-    for (let i=0; i<amount; i++){
+
+    let amount = size * size;
+    for (let i=0; i < amount; i++){
     let square = document.createElement("div");
-    square.style.backgroundColor= "silver";
+    square.addEventListener("mouseover",() => {
+        square.style.backgroundColor = "black"
+    });
+    square.style.backgroundColor= "white";
     board.insertAdjacentElement("beforeend", square);
     }
 }
@@ -21,6 +24,5 @@ function changeSize (input){
         populateBoard(input);
     } else {
         console.log("Fix the Grid")
-    }
-   
+    }  
 }
